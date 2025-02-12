3d26feecc7abe0983f96f0f2a2284c186f6439a5
Vasjan Çupri <vasjancupri04@gmail.com>
2025-02-12 14:13:52 +0100

    Added shadcn-ui with tailwindv4

--- src/utils/AxiosInstance.ts
+++ src/utils/AxiosInstance.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL;
 
 const AxiosInstance = axios.create({
-    baseURL: API_URL,
-})
+  baseURL: API_URL,
+});
 
 export default AxiosInstance;
